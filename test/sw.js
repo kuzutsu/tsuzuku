64c5c773a9012a7a6028dca34d94f17516a2f572
kuzutsu <70182575+kuzutsu@users.noreply.github.com>
2020-10-16 22:54:20 +0800

    Fix deleted service worker

--- test/sw.js
+++ test/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('v1')
             .then((cache) => cache.addAll([
-                '/',
+                'index.html',
                 'worker.js',
                 'fetchFunction.js',
                 'index.js',
