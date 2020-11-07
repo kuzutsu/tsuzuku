4f06f12efa500c1ae90ce0bf56efb12db7efa776
kuzutsu <70182575+kuzutsu@users.noreply.github.com>
2020-11-08 07:30:44 +0800

    Disable responsive layout

--- test/fetchFunction.js
+++ test/fetchFunction.js
@@ -223,7 +223,6 @@ $(function () {
                 rowFormatter: function (row) {
                     row.getElement().dataset.status = row.getData().status;
                 },
-                responsiveLayout: 'hide',
                 headerSortElement:
                     '<svg viewBox="0 0 24 24" width="17" height="17">' +
                         '<path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"></path>' +
