84c7eafaca5860507ada175292aa2688caf45a84
kuzutsu <70182575+kuzutsu@users.noreply.github.com>
2021-07-13 00:59:01 +0800

    Move to repository

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const sw = '2.3.12';
+const sw = '2.3.13';
 
 addEventListener('install', (event) => {
     event.waitUntil(
@@ -16,14 +16,14 @@ addEventListener('install', (event) => {
                 './worker.js',
 
                 // games
-                './games/',
-                './games/index.css',
-                './games/odd-one-out/',
-                './games/odd-one-out/index.css',
-                './games/odd-one-out/index.js',
-                './games/quiz/',
-                './games/quiz/index.css',
-                './games/quiz/index.js',
+                '../tsuzuku-games/',
+                '../tsuzuku-games/index.css',
+                '../tsuzuku-games/odd-one-out/',
+                '../tsuzuku-games/odd-one-out/index.css',
+                '../tsuzuku-games/odd-one-out/index.js',
+                '../tsuzuku-games/quiz/',
+                '../tsuzuku-games/quiz/index.css',
+                '../tsuzuku-games/quiz/index.js',
 
                 // images
                 './images/anilist.png',
